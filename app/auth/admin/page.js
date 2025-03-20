59d23b22784b9969cba6c5f4312a665125a859e4
Luan Rocha <lr.prazeres@bol.com.br>
2025-03-20 16:03:41 -0300

    feat:delete

--- app/auth/admin/page.js
+++ app/auth/admin/page.js
@@ -19,14 +19,6 @@ const AdminPage = () => {
       <h1 className="mb-4 text-3xl font-bold text-gray-900 dark:text-white">
         Dashboard
       </h1>
-
-      <div className="grid grid-cols-3 gap-4">
-        <AreaChartComponent />
-
-        <BarChartComponent />
-
-        <PieChartComponent />
-      </div>
     </div>
   );
 };
