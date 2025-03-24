1b63942398b5767ba35a762c8f35dc1c2b91aefd
LuanRoch <lr.prazeres@bol.com.br>
2025-03-24 17:12:17 -0300

    feat:Mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
+// next.config.mjs
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obter __dirname em ES Modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "fakestoreapi.com",
-        },
-      ],
-    },
-  };
-  const path = require('path')
-
-module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+      },
+    ],
+  },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname)
-    return config
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   }
-}
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
