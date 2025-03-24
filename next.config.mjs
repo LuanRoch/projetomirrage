cf8b5e9e2bedb72e05ec510b7ece6816ed1e58de
LuanRoch <lr.prazeres@bol.com.br>
2025-03-24 17:07:17 -0300

    feat:updateError1

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
       ],
     },
   };
+  const path = require('path')
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  }
+}
   
   export default nextConfig;
   
